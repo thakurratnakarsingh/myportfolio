c5e64e5efcd0d7b5477e0a312db77b95cadc6eba
Ratnakar Singh <thakursingh294t@gmail.com>
2025-04-19 12:43:13 +0530

    Adding of the nav bar

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -2,27 +2,31 @@ import React from "react";
 import "../Styles/TeachnologyStack.css";
 import ca from "../Assets/ca.png";
 import reacticon from "../Assets/dmce.png";
-
+import Nevbar from './Layout/Nevbar';
+import Footer from './Layout/Footer';
 
 const Education = () => {
   return (
-    
-    <div className="tech-container">
-      <h2>Academic year</h2>
-      <div className="stack-icons">
-        <view>
-          <img src={reacticon} alt="GitHub" className="icon" />
-          <h3>UNITED COLLEGE OF</h3> 
-          <h3> ENGINEERING AND MANAGEMENT</h3>
-          <p>August 2015 to July 2019</p>
-        </view>
-        <view>
-          <img src={ca} alt="React" className="icon" />
-          <h3>CENTRAL ACADEMY</h3> 
-          <h3>SENIOR SECONDARY SCHOOL</h3>
-          <p >2013-2015</p>
-       </view>
-     </div>
+    <div>
+      <Nevbar />
+      <div className="tech-container">
+        <h2>Academic year</h2>
+        <div className="stack-icons">
+          <view>
+            <img src={reacticon} alt="GitHub" className="icon" />
+            <h3>UNITED COLLEGE OF</h3> 
+            <h3> ENGINEERING AND MANAGEMENT</h3>
+            <p>August 2015 to July 2019</p>
+          </view>
+          <view>
+            <img src={ca} alt="React" className="icon" />
+            <h3>CENTRAL ACADEMY</h3> 
+            <h3>SENIOR SECONDARY SCHOOL</h3>
+            <p>2013-2015</p>
+          </view>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import "../Styles/TeachnologyStack.css";
 import Project1 from "../Assets/Project1.png";
+import Nevbar from './Layout/Nevbar';
+import Footer from './Layout/Footer';
 
-const Project=()=>{
+const Project = () => {
     return (
+        <div>
+            <Nevbar />
             <div className="tech-container">
-              <h2 className="tech-title">Projects</h2>
-              <div className="stack-icons">
-                <img src={Project1} alt="GitHub" className="icon" />
-                <img src={Project1} alt="GitHub" className="icon" />
-                <img src={Project1} alt="GitHub" className="icon" />
-              </div>
+                <h2 className="tech-title">Projects</h2>
+                <div className="stack-icons">
+                    <img src={Project1} alt="GitHub" className="icon" />
+                    <img src={Project1} alt="GitHub" className="icon" />
+                    <img src={Project1} alt="GitHub" className="icon" />
+                </div>
             </div>
-          );
+            <Footer />
+        </div>
+    );
+}
 
-        }
-export default Project
+export default Project;
 
